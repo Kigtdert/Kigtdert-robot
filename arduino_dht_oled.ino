76d031c19e783a307c30fcc9ad6a873615cd6e46
Kigtdert <150541685+Kigtdert@users.noreply.github.com>
2024-04-03 15:45:12 +0200

    Update arduino_dht_oled.ino

--- arduino_dht_oled.ino
+++ arduino_dht_oled.ino
@@ -3,55 +3,45 @@
 #include <Adafruit_SSD1306.h>
 #include <DHT.h>
 #include <SoftwareSerial.h>
-SoftwareSerial Serial2(2, 3);
+#include <RCSwitch.h>
 
-#define OLED_WIDTH 128  // Šířka displeje v pixelech
-#define OLED_HEIGHT 64  // Výška displeje v pixelech
-#define OLED_RESET -1   // Reset pin pro displej
-#define DHT_PIN 11      // Pin pro DHT11
-#define DHT_TYPE DHT11  // Typ senzoru
-
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
-DHT dht(DHT_PIN, DHT_TYPE);
+SoftwareSerial Serial2(2, 3);       //piny 2 a 3
+Adafruit_SSD1306 display(128,64, &Wire, -1);
+DHT dht(11, DHT11);       //pin 11
+RCSwitch mySwitch = RCSwitch();
 
 void dopredu() {
   digitalWrite(8, HIGH);
   digitalWrite(6, LOW);
   digitalWrite(7, HIGH);
   digitalWrite(9, LOW);
-  Serial.println("dopredu");
 }
 void dozadu() {
   digitalWrite(8, LOW);
   digitalWrite(6, HIGH);
   digitalWrite(7, LOW);
   digitalWrite(9, HIGH);
-  Serial.println("dozadu");
 }
 void doprava() {
   digitalWrite(6, HIGH);
   digitalWrite(7, LOW);
   digitalWrite(8, LOW);
   digitalWrite(9, HIGH);
-  Serial.println("doprava");
 }
 void doleva() {
   digitalWrite(7, HIGH);
   digitalWrite(6, LOW);
   digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
-  Serial.println("doleva");
 }
 void zastavit() {
   digitalWrite(6, LOW);
   digitalWrite(7, LOW);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
-  // zastavit
 }
 void setup() {
-  Serial.begin(9600);
-  pinMode(12, OUTPUT);  //piezo
+  pinMode(4, OUTPUT);   //piezo
   pinMode(10, OUTPUT);  //červená ledka
   pinMode(6, OUTPUT);   //motory
   pinMode(7, OUTPUT);
@@ -59,12 +49,10 @@ void setup() {
   pinMode(9, OUTPUT);
   pinMode(5, INPUT);    //tlacitko vedle displaye
   pinMode(A3,INPUT);    //napětí baterie
-  pinMode(A2,INPUT);    // termistor 1
-  pinMode(A1,INPUT);    // termistor 2
-  pinMode(A0,INPUT);    // termistor 3
+  mySwitch.enableTransmit(12); //433Mhz vysílač
   
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println("OLED displej není nalezen!");
+    //Serial.println("OLED displej není nalezen!");
     while (true);
   }
   
@@ -74,24 +62,23 @@ void setup() {
   Serial2.begin(9600);
   zastavit();
   display.clearDisplay();
+  display.display();
   display.setCursor(0, 0);
   display.print("Kigtdert");
   display.setCursor(0, 16);
   display.print("robotics");
   display.display();
-  digitalWrite(12, HIGH);
+  digitalWrite(4, HIGH);
   delay(250);
-  digitalWrite(12, LOW);
+  digitalWrite(4, LOW);
   delay(250);
-  digitalWrite(12, HIGH);
+  digitalWrite(4, HIGH);
   delay(250);
-  digitalWrite(12, LOW);
+  digitalWrite(4, LOW);
 }
 
 void loop() {
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  int x = digitalRead(5);
+  int x = digitalRead(5); //kapacitní tlačítko
   if (x == 1)
   {
       display.clearDisplay();
@@ -108,10 +95,10 @@ void loop() {
       int baterie = analogRead(A3);
       if (baterie<700){
         display.print("LOW");
-        digitalWrite(12, HIGH);
+        digitalWrite(4, HIGH);
         digitalWrite(10, HIGH);
         delay(100);
-        digitalWrite(12, LOW);
+        digitalWrite(4, LOW);
         digitalWrite(10, LOW);
         delay(100);
       }
@@ -121,7 +108,9 @@ void loop() {
       display.display();
   }
   else{
-    display.clearDisplay();
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  display.clearDisplay();
   display.setCursor(0, 0);
   display.print("Teplota:");
   display.setCursor(0, 16);
@@ -136,70 +125,62 @@ void loop() {
   display.display();
   }
   
-  float napeti1 = analogRead(A1);
-  float napeti2 = analogRead(A2);
-  float napeti3 = analogRead(A0); 
-
-  while (napeti1>800 or napeti2>800 or napeti3>800)      //hodnota napeti se musí otestovat podle realných teplot a odporů
-  {
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print("Overheat");
-    display.setCursor(0, 16);
-    display.print("error");
-    display.display();
-    digitalWrite(10,HIGH);
-    delay(500);
-    digitalWrite(10,LOW);
-    delay(500);
-    float napeti1 = analogRead(A1);
-    float napeti2 = analogRead(A2);
-    float napeti3 = analogRead(A0);
-    }
   
 
 while (!Serial2.available()) {
 
 }
-    String zprava = Serial2.readString();
-    String zprava2 = zprava.substring(0,1);
+    String zprava1 = Serial2.readString();
+    String zprava2 = zprava1.substring(0,1);
     int motory1=zprava2.toInt();
-    Serial.println(motory1);
     
-    switch (motory1) {
+switch (motory1) {
   case 1:
-    Serial.println("dopredu");
-    dopredu();
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print("IP adresa: ");
+    display.setCursor(0,16);
+    display.print(zprava1);//ip adresa serveru
+    display.display();
+    delay(10000);
     break;
-  case 2:
-    Serial.println("dozadu");
-    dozadu();
-    break;
-   case 3:
-    Serial.println("doprava");
-    doprava();
-    break;
-  case 4:
-    Serial.println("doleva");
+  case 2: 
     doleva();
     break;
-  case 5:
-    Serial.println("stop");
+  case 3: 
     zastavit();
     break;
-   case 6:
-    Serial.println("beep");
-    digitalWrite(12,HIGH);
+  case 4:   
+    doprava();
+    break;
+  case 5:   
+    dozadu();
+    break;
+  case 6:   
+    digitalWrite(4,HIGH);
     delay(200);
-    digitalWrite(12,LOW);
+    digitalWrite(4,LOW);
     break;
   case 7:
-    String text = zprava.substring(1);
+    dopredu();
+    break;
+  case 8:   
+    String text = zprava1.substring(1);
     display.clearDisplay();
-    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.setCursor(0,0);
     display.print(text);
     display.display();
-    delay(5000);
+    display.setTextSize(2);
+    break;
+  case 9:   
+    String radio = zprava1.substring(1);
+    if(radio == "25"){
+      mySwitch.send(5393, 24); 
+        }
+    else if(radio == "28"){
+      mySwitch.send(5398, 24); 
+        }
     break;
 }
 }
